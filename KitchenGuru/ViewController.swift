a20ccfe1a259e7db359a385d0d8d6d549f5f49d1
Nicolas Ricci <nricci.02@gmail.com>
2017-06-20 10:55:58 -0300

    Initial Commit

---
+++ KitchenGuru/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KitchenGuru
+//
+//  Created by Nicolas Ricci on 20/6/17.
+//  Copyright © 2017 Nicolas Ricci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
