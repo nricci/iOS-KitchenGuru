eefa2a36224e34ca6424b692bb5f695e9fd9092b
Nicolas Ricci <nricci.02@gmail.com>
2017-06-20 20:35:33 -0300

    Initial commit. Food table, and custom cells.

---
+++ KitchenGuru/TableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  TableViewCell.swift
+//  KitchenGuru
+//
+//  Created by Nicolas Ricci on 20/6/17.
+//  Copyright © 2017 Nicolas Ricci. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var title: UILabel!
+    
+    @IBOutlet weak var ingredients: UILabel!
+    
+    @IBOutlet weak var thumbnail: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- KitchenGuru/ViewController.swift
+++ KitchenGuru/ViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+var meals : [[String:Any]] = []
 
+class ViewController: UITableViewController {
+    
+    @IBOutlet var table: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +22,54 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return meals.count
+    }
+   
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+        
+        // Populating cell
+        let meal = meals[indexPath.row]
+        cell.title.text = meal["name"] as! String
+        cell.ingredients.text = (meal["ingredients"] as! [String]).joined(separator: ", ")
+        let url = URL(string: meal["image_url"] as! String)
+        getDataFromUrl(url: url!) { (data, response, error)  in
+            guard let data = data, error == nil else { return }
+            //print(response?.suggestedFilename ?? url.lastPathComponent)
+            //print("Download Finished")
+            DispatchQueue.main.async() { () -> Void in
+                cell.thumbnail.image = UIImage(data: data)
+            }
+        }
+        
+        return cell
+    }
+    
+    func getDataFromUrl(url: URL, completion: @escaping (_ data: Data?, _  response: URLResponse?, _ error: Error?) -> Void) {
+        URLSession.shared.dataTask(with: url) {
+            (data, response, error) in
+            completion(data, response, error)
+            }.resume()
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let url = URL(string: "https://private-d3acc-mealsapi1.apiary-mock.com/meals")
+        URLSession.shared.dataTask(with:url!, completionHandler: {(data, response, error) in
+            guard let data = data, error == nil else { return }
+            
+            do {
+                meals = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [[String:Any]]
+                self.table.reloadData()
+            } catch let error as NSError {
+                print(error)
+            }
+        }).resume()
+        
     }
 
 
